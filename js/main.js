22fa5f1f534659ce7c1a45307a83f5077dc22dc9
ohidulalam <mdnoyonctg15@gmail.com>
2021-09-02 16:36:45 +0600

    almost added

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const searchFiled = document.getElementById('search-field');
 const bookContainer = document.getElementById('book-container');
 const searchDataCount = document.getElementById('data-count');
 const searchError = document.getElementById('sesrch-error');
+
 /* Add event handeler */
-searchbtn.addEventListener('click', function(){
+searchbtn.addEventListener('click', function () {
     const search = searchFiled.value;
 
     /* search value clear */
@@ -14,19 +15,19 @@ searchbtn.addEventListener('click', function(){
     /* search books data fetch */
     const url = `http://openlibrary.org/search.json?q=${search}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayShow(data.docs))
+        .then(res => res.json())
+        .then(data => displayShow(data.docs))
 });
 
 /* Display show books data */
 const displayShow = books => {
-    
+
     /* Result Error Handling */
-    if(books.length === 0){
+    if (books.length === 0) {
         searchError.innerHTML = `<h2 class= "fw-bold text-center text-danger my-5">Not Result Found</h2>`;
         searchDataCount.innerHTML = '';
     }
-    else{
+    else {
         searchError.innerHTML = '';
     };
     /* data dom clear */
@@ -38,7 +39,7 @@ const displayShow = books => {
 
         /* data result count */
         searchDataCount.innerHTML = `
-        <h5 class="fw-bold">Search results found: ${books.length}</h5>`;
+        <h5 class="fw-bold">Search Books Found: ${books.length}</h5>`;
 
         /* data is added to the container */
         const div = document.createElement('div');
@@ -48,13 +49,13 @@ const displayShow = books => {
             <img class: "img-fluid" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid"">
             <div class="card-body">
                 <h5 class="card-title fw-bold">Name: ${book.title}</h5>
-                <p class="card-text">Publisher: ${book.publisher}</p>
-                <p class="card-text">First publish year: ${book.first_publish_year}</p>
+                <p>Publisher: ${book.publisher ? book.publisher: 'Not found'}</p>
+                <p>Author Name: ${book.author_name ? book.author_name: 'Not found'}</p>
+                <p>First publish year: ${book.first_publish_year}</p>
             </div>
         </div>
         `;
         bookContainer.appendChild(div);
     })
-        
-}; 
-{/* <p class="ard-text">Author Name: ${book.author_name[0]}</p> */}
+
+};
