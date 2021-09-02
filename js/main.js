f4227609776c06d0c0ad057e52380f79b2378082
ohidulalam <mdnoyonctg15@gmail.com>
2021-09-02 21:17:19 +0600

    added

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const searchbtn = document.getElementById('search-button')
 const searchFiled = document.getElementById('search-field');
 const bookContainer = document.getElementById('book-container');
 const searchDataCount = document.getElementById('data-count');
-const searchError = document.getElementById('sesrch-error');
+const searchError = document.getElementById('search-error');
 
 /* Add event handeler */
 searchbtn.addEventListener('click', function () {
     const search = searchFiled.value;
 
     /* search value clear */
-    searchFiled.value = '';
+    searchFiled.value = '';  
 
     /* search books data fetch */
     const url = `https://openlibrary.org/search.json?q=${search}`;
@@ -31,7 +31,7 @@ const displayShow = data => {
 
     /* data result found */    
     const books = data.docs;
-
+    
     /* Result Error Handling */
     if (books.length === 0) {
         searchError.innerHTML = `<h2 class= "fw-bold text-center text-danger my-5">Not Result Found</h2>`;
@@ -43,7 +43,7 @@ const displayShow = data => {
 
     /* Loop run to get data */
     books.forEach(book => {
-
+        
     /* data is added to the container */
         const div = document.createElement('div');
         div.classList.add('col');
@@ -60,5 +60,4 @@ const displayShow = data => {
         `;
         bookContainer.appendChild(div);
     });
-
 };
